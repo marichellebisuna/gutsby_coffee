0113b653615b86e735a36f7ed399b3b6def5e5a6
Marichelle Bisuna <marichellebisuna@yahoo.com>
2019-02-26 10:52:47 +1100

    first commit

---
+++ src/components/Global/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import logo from '../../images/logo.svg';
+import { FaCartArrowDown } from 'react-icons/fa';
+
+export default class Navbar extends Component {
+	render() {
+		return <div>Hello from Navbar</div>;
+	}
+}
